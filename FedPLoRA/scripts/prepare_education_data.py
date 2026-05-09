284341a0819072ad3f5100c0e6bf8cef2d6f4000
roamBo <1687352409@qq.com>
2026-05-09 21:46:08 +0800

    merge gb version

--- FedPLoRA/scripts/prepare_education_data.py
+++
@@ -1,9 +0,0 @@
-from prepare_hf_domain_data import main
-
-
-if __name__ == "__main__":
-    main(
-        default_domain="education",
-        default_dataset="ScaleAI/TutorBench",
-        description="Prepare education-domain SFT data into FedPLoRA JSONL.",
-    )
